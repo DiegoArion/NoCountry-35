6c4b886ba595c1d271110ea682a94d6c995e3a76
DiegoArion <70103662+DiegoArion@users.noreply.github.com>
2023-02-18 16:00:42 -0600

    ADD -- Adding files to the repo

---
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce;
+
+import com.example.ecommerce.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ecommerce/model/Role.java
@@ -0,0 +1,22 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
